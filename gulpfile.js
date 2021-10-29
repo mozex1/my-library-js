7d3a587b9ce1692bf4569b23bfd6341e77a48e98
Timur <86473514+mozex1@users.noreply.github.com>
2021-10-29 22:47:48 +0300

    architecture

--- gulpfile.js
+++ gulpfile.js
@@ -8,7 +8,6 @@ const autoprefixer = require("autoprefixer");
 const cleanCSS = require("gulp-clean-css");
 const postcss = require("gulp-postcss");
 
-// const dist = "/Applications/MAMP/htdocs/test"; // Ссылка на вашу папку на локальном сервере
 const dist = "./dist";
 
 gulp.task("copy-html", () => {
