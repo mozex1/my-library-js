fc78658bb8ec77a3cb175f57779bd35ffc949884
Timur <86473514+mozex1@users.noreply.github.com>
2021-11-02 23:38:08 +0300

    add fadeToggle and Dropdown menu

--- src/js/main.js
+++ src/js/main.js
@@ -1,15 +1,18 @@
 import './lib/lib';
 
-$('#first').on('click', () => {
-    $('div').eq(1).fadeOut(800);
-});
+// $('.wrap').html(
+//     `
+//     <div class="dropdown">
+//         <button class="btn btn-primary dropdown-toggle" id="dropdownMenuButton" >Dropdown button</button>
+//         <div class="dropdown-menu" data-toggle-id="dropdownMenuButton">
+//             <a href="#" class="dropdown-item">Action 1</a>
+//             <a href="#" class="dropdown-item">Action 2</a>
+//             <a href="#" class="dropdown-item">Action 3</a>
+//         </div>
+//     </div>
+//     `
+// );
 
-$('[data-count="second"]').on('click', () => {
-    $('div').eq(2).fadeOut(800);
-});
 
-$('button').eq(2).on('click', () => {
-    $('.w-500').fadeOut(800);
-});
 
 
