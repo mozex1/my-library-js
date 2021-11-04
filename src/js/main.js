cb63b3c2557b055928773e990b8fadb88699d70b
Timur <86473514+mozex1@users.noreply.github.com>
2021-11-04 12:39:23 +0300

    add dynamic create modal

--- src/js/main.js
+++ src/js/main.js
@@ -1,18 +1,29 @@
 import './lib/lib';
 
-// $('.wrap').html(
-//     `
-//     <div class="dropdown">
-//         <button class="btn btn-primary dropdown-toggle" id="dropdownMenuButton" >Dropdown button</button>
-//         <div class="dropdown-menu" data-toggle-id="dropdownMenuButton">
-//             <a href="#" class="dropdown-item">Action 1</a>
-//             <a href="#" class="dropdown-item">Action 2</a>
-//             <a href="#" class="dropdown-item">Action 3</a>
-//         </div>
-//     </div>
-//     `
-// );
-
+$('#trigger').click(() => $('#trigger').createModal({
+    text: {
+        title: 'Modal title',
+        body: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Harum minus doloremque nesciunt enim rem quam corporis? Dolorem pariatur magnam distinctio perferendis. Ratione dolorem voluptates iusto facilis odit veritatis, suscipit voluptatibus!'
+    },
+    btns: {
+        count: 2,
+        settings: [
+            [
+                'Close',
+                ['btn-danger', 'mr-10'],
+                true
+            ],
+            [
+                'Save changes',
+                ['btn-success'],
+                false,
+                () => {
+                    alert('Данные сохранены');
+                }
+            ]
+        ]
+    }
+}));
 
 
 
