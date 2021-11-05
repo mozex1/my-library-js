82eba1e32ed164b7b3feae17bb273d56eeac6153
Timur <86473514+mozex1@users.noreply.github.com>
2021-11-05 15:31:47 +0300

    dist > prod vers

--- src/js/main.js
+++ src/js/main.js
@@ -1,32 +1,5 @@
-import './lib/lib';
-
-$('#trigger').click(() => $('#trigger').createModal({
-    text: {
-        title: 'Modal title',
-        body: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Harum minus doloremque nesciunt enim rem quam corporis? Dolorem pariatur magnam distinctio perferendis. Ratione dolorem voluptates iusto facilis odit veritatis, suscipit voluptatibus!'
-    },
-    btns: {
-        count: 2,
-        settings: [
-            [
-                'Close',
-                ['btn-danger', 'mr-10'],
-                true
-            ],
-            [
-                'Save changes',
-                ['btn-success'],
-                false,
-                () => {
-                    alert('Данные сохранены');
-                }
-            ]
-        ]
-    }
-}));
+import $ from './lib/lib';
 
 
-$().get('https://jsonplaceholder.typicode.com/todos/1')
-    .then(res => console.log(res));
 
 
